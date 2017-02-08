c57c595ad529b93fd913caf5bd934416bbde4730
Dinakar Tumu <dinakar91@gmail.com>
2017-02-07 19:32:01 -0800

    Finished basic layout for tools section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 import Projects from './Projects.jsx';
+import Tools from './Tools.jsx';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
+				<Tools />
 				<Projects />
 			</div>
 			
